a89bcbffc7de2df8e182d24c536d2aafe37a5883
Daniel Glauser <danglauser@gmail.com>
2014-11-26 17:28:29 +0000

    Updated for the latest cider.

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives
              '("marmalade" .
                "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 ;; Don't want none of them files without a dangling newline now
@@ -9,28 +11,16 @@
 
 (add-to-list 'load-path "~/.emacs.d/modes/clojure-mode")
 (require 'clojure-mode)
-
-(add-to-list 'load-path "~/.emacs.d/vendor/nrepl")
-(require 'nrepl)
-(require 'clojure-test-mode)
+(add-to-list 'load-path "~/.emacs.d/modes/cider")
+(require 'cider)
+;;https://github.com/iantruslove/emacs.d/blob/master/site-lisp/clojure-test-toggle/clojure-test-toggle.el
+(add-to-list 'load-path "~/.emacs.d/modes/clojure-test-toggle")
+(require 'clojure-test-toggle)
 
 (eval-after-load 'clojure-mode
   '(progn
      ;;(remove-hook 'clojure-mode-hook 'esk-pretty-fn)
      (remove-hook 'slime-indentation-update-hooks 'put-clojure-indent)))
-
-(let ((sonian-nav-file "~/sonian/sa-safe/.elisp/sonian-navigation.el"))
-  (when (file-exists-p sonian-nav-file)
-    (load (expand-file-name sonian-nav-file))))
-
-(defun run-ns-tests ()
-  (interactive)
-  (nrepl-load-current-buffer)
-  (clojure-jump-to-test)
-  (nrepl-load-current-buffer)
-  (clojure-test-run-tests)
-  (quit-window))
-(define-key clojure-mode-map (kbd "C-c ,") 'run-ns-tests)
 
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
@@ -79,9 +69,7 @@
 
 (require 'zenburn-theme)
 (load-theme 'zenburn t)
-
-(require 'paren)
-(eval-after-load 'zenburn-theme
+(eval-after-load 'paren
   '(setq show-paren-style 'expression))
 
 (custom-theme-set-faces
@@ -94,5 +82,70 @@
 
 (global-git-gutter-mode t)
 
-(add-hook 'nrepl-mode-hook 'paredit-mode) ; for nrepl.el <= 0.1.8
-(add-hook 'nrepl-repl-mode-hook 'paredit-mode) ; for nrepl.el > 0.1.8
+;;(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;;; Undo buffer until reverted
+;; Inspired by: http://www.emacswiki.org/emacs/RevertBuffer
+(defun undo-until-reverted ()
+  "Undo all edits until the buffer is unmodified."
+  (interactive)
+  (when (buffer-modified-p)
+    (undo))
+  (while (buffer-modified-p)
+    (undo-more 1))
+  (message "Buffer was undone until no longer modified."))
+
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(add-hook 'cider-mode-hook
+          (lambda ()
+            (define-key cider-mode-map (kbd "C-c z") 'cider-switch-to-relevant-repl-buffer)))
+(add-hook 'cider-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+(eval-after-load 'cider
+  '(setq cider-repl-history-size 10000
+         cider-repl-history-file "~/.cider.history.el"))
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(setq cider-show-error-buffer 'except-in-repl)
+
+;; ///////////////////////////////////////////////////////////////////////////////////
+;;https://github.com/iantruslove/emacs.d/blob/master/lisp/init-clojure-cider.el#L11-38
+;;(add-hook 'cider-repl-mode-hook
+;;'set-auto-complete-as-completion-at-point-function)
+;;(add-hook 'cider-mode-hook
+;;'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'subword-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+;;(define-key cider-mode-map (kbd "C-c C-d")
+;;'ac-nrepl-popup-doc)
+(define-key cider-mode-map (kbd "C-c z") 'cider-switch-to-relevant-repl-buffer)
+
+;; nrepl isn't based on comint
+(add-hook 'cider-repl-mode-hook
+          (lambda () (setq show-trailing-whitespace nil)))
+
+(add-hook 'cider-mode-hook
+          (lambda ()
+            (define-key cider-mode-map (kbd "C-c t") 'cider-test-show-report)
+            (define-key cider-mode-map (kbd "C-c C-t") nil)))
+
+(let ((sonian-stuff "~/sonian/sa-safe/.elisp/sonian.el"))
+  (when (file-exists-p sonian-stuff)
+    (message "Loading Sonian extras...")
+    (load (expand-file-name sonian-stuff))
+    (require 'sonian)
+    ;; Turn on whitespace mode all
+    ;; the time
+    (add-hook 'clojure-mode-hook 'whitespace-mode)))
+
+;;https://github.com/iantruslove/emacs.d/blob/master/lisp/init-clojure.el#L32-39
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (define-key clojure-mode-map (kbd "C-c C-t") 'clojure-test-toggle)))
